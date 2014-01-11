76da19740c44127c334d8037b5bde449758c7c4b
shishir shetty <shishirshetty0710@gmail.com>
2014-01-11 16:00:06 +0530

    gps code ticket screen centrail railway webservice added

--- GpsBasedOnlineTicket/src/com/college/gpsbasedonlineticket/CentralRail.java
+++ GpsBasedOnlineTicket/src/com/college/gpsbasedonlineticket/CentralRail.java
@@ -3,18 +3,31 @@ package com.college.gpsbasedonlineticket;
 
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 
+import com.bg.task.FareCalculator;
+import com.bg.task.UiHandler;
+import com.data.Place;
+
 public class CentralRail extends Activity
 {
+	Context context;
+	
 	Spinner sp_source,sp_dest;
 	Button proceed,clear;
 	RadioGroup rgGroup;
 	RadioButton rbButton_single,rbtnButton_return;
+	
+	Button btn_submit_ticket;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -22,15 +35,82 @@ public class CentralRail extends Activity
 		setContentView(R.layout.central_rail);
 		
 		init();
+		
+		proceed.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				uihandler.sendEmptyMessage(UiHandler.HIT_WEBSERVICE_REQUEST_FARE_DETAILS);
+			}
+		});
+		
+		
+		btn_submit_ticket.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(context, TicketScreen.class));
+				finish();
+			}
+		});
 	}
 	
 	public void init()
 	{
-		sp_source = (Spinner) findViewById(R.id.sp_central);
-		sp_dest = (Spinner) findViewById(R.id.sp_central);
+		sp_source = (Spinner) findViewById(R.id.sp_central_from);
+		sp_dest = (Spinner) findViewById(R.id.sp_central_to);
 		rgGroup = (RadioGroup) findViewById(R.id.btn_rg);
 		rbButton_single = (RadioButton) findViewById(R.id.rbtn_single);
 		rbtnButton_return = (RadioButton) findViewById(R.id.rbtn_return);
 		
+		proceed=(Button)findViewById(R.id.btn_proceed);
+		
+		context = CentralRail.this;
+		uihandler.setApplicationContext(context);
+		
+		btn_submit_ticket=(Button)findViewById(R.id.btn_submit_ticket);
 	}
+	
+	
+	UiHandler uihandler = new UiHandler(){
+		
+		public void handleMessage(android.os.Message msg) {
+			
+			switch (msg.what) {
+			case HIT_WEBSERVICE_REQUEST_FARE_DETAILS:
+			
+				Place placeObj= new Place();
+				placeObj.from = sp_source.getSelectedItem().toString();
+				placeObj.to = sp_dest.getSelectedItem().toString();
+				
+				if(rbButton_single.isChecked()){
+					placeObj.way = true;
+				}else placeObj.way = false;
+				
+				new FareCalculator(context, uihandler, placeObj).execute();
+				break;
+				
+			case HIT_WEBSERVICE_PROCESS_FARE_DETAILS:
+				
+				if (msg != null && msg.obj != null) {
+					
+					Place  placeDetails = (Place) msg.obj;
+					if(placeDetails!=null){
+						Log.e("newlog", placeDetails.toString());
+					}
+				}
+				
+				break;
+
+			default:
+				break;
+			}
+			
+			
+		};
+		
+		
+	};
 }
